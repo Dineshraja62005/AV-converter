13359cfc7c91fa52abe0027e453a9b43012e5776
Dineshraja62005 <dineshraja62005@gmail.com>
2025-05-07 03:52:13 +0530

    modified:   infra/variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,4 +3,7 @@ variable "aws_secret_key" {}
 variable "key_name" {}
 variable "private_key_path" {}
 variable "aws_session_token" {}
-
+variable "ec2_host" {
+  description = "Public DNS or IP of the manually created EC2 instance"
+  type        = string
+}
