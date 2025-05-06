d49455670bfe3571c4edfbae5ef1409d5c1528b3
Dineshraja62005 <dineshraja62005@gmail.com>
2025-05-07 03:34:12 +0530

    modified:   .github/workflows/ci.yml 	modified:   infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+# Provider configuration
 provider "aws" {
   region     = "us-east-1"
   access_key = var.aws_access_key
@@ -5,7 +6,15 @@ provider "aws" {
   token      = var.aws_session_token
 }
 
+# Data source to reference an existing security group by name
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["av_converter_sg"]  # Name of the existing security group
+  }
+}
 
+# EC2 instance resource using the existing security group
 resource "aws_instance" "av_ec2" {
   ami           = "ami-0c2b8ca1dad447f8a" # Ubuntu 22.04 LTS (update as needed)
   instance_type = "t2.medium"
@@ -15,7 +24,8 @@ resource "aws_instance" "av_ec2" {
     Name = "av-converter"
   }
 
-  vpc_security_group_ids = [aws_security_group.av_sg.id]
+  # Use the existing security group found using the data source
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
   provisioner "remote-exec" {
     inline = [
@@ -29,28 +39,19 @@ resource "aws_instance" "av_ec2" {
       private_key = file(var.private_key_path)
       host        = self.public_ip
     }
+
+    depends_on = [data.aws_security_group.existing_sg]
   }
 }
 
-resource "aws_security_group" "av_sg" {
-  name        = "av_converter_sg"
-  description = "Allow SSH and web ports"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Variables definition (example)
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_session_token" {}
+variable "private_key_path" {}
+variable "key_name" {}
+
+# Outputs (optional)
+output "instance_public_ip" {
+  value = aws_instance.av_ec2.public_ip
 }
