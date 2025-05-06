192cad68a2335bc70e3a666b31eddbc6ae8c08fa
Dineshraja62005 <dineshraja62005@gmail.com>
2025-05-07 04:26:24 +0530

    modified:   infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -21,11 +21,20 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# Get default VPC info
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_security_group" "existing_sg" {
+  name = "av_converter_sg"
+}
+
 resource "aws_instance" "av_ec2" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.medium"
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.av_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
   root_block_device {
     volume_size = 20
@@ -53,35 +62,36 @@ resource "aws_instance" "av_ec2" {
   }
 }
 
-resource "aws_security_group" "av_sg" {
-  name        = "av_converter_sg"
-  description = "Allow SSH and application port access"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH access"
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Application access"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "av-converter-security-group"
-  }
-}
+# Remove the security group resource since we're using an existing one
+# resource "aws_security_group" "av_sg" {
+#   name_prefix = "av_converter_sg_"
+#   description = "Allow SSH and application port access"
+#
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "SSH access"
+#   }
+#
+#   ingress {
+#     from_port   = 5000
+#     to_port     = 5000
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Application access"
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allow all outbound traffic"
+#   }
+#
+#   tags = {
+#     Name = "av-converter-security-group"
+#   }
+# }
